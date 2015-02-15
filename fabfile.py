f632e3846da3e122bd2a93f9ab90c520c9accdab
David Simic <dpsimic@gmail.com>
2015-02-15 15:33:00 -0500

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,9 @@
+from fabric.contrib.files import exists
 from fabric.api import env, run, sudo, put
 
 env.use_ssh_config = True
-
-
-env.gitosis_src_dir = '~/src/gitosis'
+env['sudo_prefix'] += '-H '
+env.gitosis_src_dir = './src/gitosis'
 
 
 def install_deps():
@@ -11,26 +11,40 @@ def install_deps():
 
 
 def install_gitosis():
-    run('mkdir -p %s' % env.gitosis_src_dir)
-    run('cd %s; python setup.py install' % env.gitosis_src_dir)
+    if not exists(env.gitosis_src_dir):
+        run('cd; mkdir -p %s' % env.gitosis_src_dir)
+        run('cd; cd %s/../; git clone https://github.com/tv42/gitosis.git' %
+            env.gitosis_src_dir)
+        sudo('cd; cd %s; python setup.py install --record files.txt' %
+             env.gitosis_src_dir)
     # create git user
-    sudo('adduser --system ' +
-         '--shell /bin/sh ' +
-         '--gecos ' +
-         '--group ' +
-         '--disabled-password ' +
-         '--home /home/git ' +
-         'git'
-         )
-    # ensure gitosis post-update hook executable
-    sudo('chmod 755 /home/git/repositories/gitosis-admin.git/hooks/post-update')
+    if not exists('/home/git'):
+        sudo('adduser --system ' +
+             '--shell /bin/sh ' +
+             '--gecos ' +
+             '--group ' +
+             '--disabled-password ' +
+             '--home /home/git ' +
+             'git'
+             )
 
 
-def add_git_pubkey(pubkey_name):
+def pubkey(pubkey_name):
     """
     Call as:
         fab -H host1:user1, host2:user2 add_git_pubkey:pubkey_name
     """
     # add pub key
     put('~/.ssh/%s.pub' % pubkey_name, '/tmp/%s.pub' % pubkey_name)
-    sudo('-H -u git gitosis-init < /tmp/%s.pub' % pubkey_name)
+    sudo('gitosis-init < /tmp/%s.pub' % pubkey_name, user="git")
+    # ensure gitosis post-update hook executable
+    sudo('chmod 755 /home/git/repositories/gitosis-admin.git/hooks/post-update')
+
+
+def teardown():
+    pass
+
+
+def deploy():
+    install_deps()
+    install_gitosis()
